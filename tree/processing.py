c493aea2a8499ca3629e0f24652d6e1171ae76d9
mostobriv <mostobriv@gmail.com>
2021-06-22 18:42:48 +0700

    moved some constants to tree/consts.py

--- tree/processing.py
+++ tree/processing.py
@@ -1,6 +1,10 @@
 from __future__ import print_function
 import idaapi
 
+idaapi.require('tree.consts')
+
+from tree.consts import binary_expressions_ops, unary_expressions_ops, op2str, str2op
+
 
 DEBUG_PRINT_ENABLED = False
 def debug_print(*args, **kwargs):
@@ -14,70 +18,6 @@ def debug_print(*args, **kwargs):
         _args = args
         print("[%s]" % message, *_args)
 
-# [NOTE]: Actual for 7.6
-op2str = {
-    0: 'cot_empty', 1: 'cot_comma', 2: 'cot_asg', 
-    3: 'cot_asgbor', 4: 'cot_asgxor', 5: 'cot_asgband', 
-    6: 'cot_asgadd', 7: 'cot_asgsub', 8: 'cot_asgmul', 
-    9: 'cot_asgsshr', 10: 'cot_asgushr', 11: 'cot_asgshl',
-    12: 'cot_asgsdiv', 13: 'cot_asgudiv', 14: 'cot_asgsmod',
-    15: 'cot_asgumod', 16: 'cot_tern', 17: 'cot_lor',
-    18: 'cot_land', 19: 'cot_bor', 20: 'cot_xor',
-    21: 'cot_band', 22: 'cot_eq', 23: 'cot_ne',
-    24: 'cot_sge', 25: 'cot_uge', 26: 'cot_sle',
-    27: 'cot_ule', 28: 'cot_sgt', 29: 'cot_ugt',
-    30: 'cot_slt', 31: 'cot_ult', 32: 'cot_sshr',
-    33: 'cot_ushr', 34: 'cot_shl', 35: 'cot_add',
-    36: 'cot_sub', 37: 'cot_mul', 38: 'cot_sdiv', 
-    39: 'cot_udiv', 40: 'cot_smod', 41: 'cot_umod', 
-    42: 'cot_fadd', 43: 'cot_fsub', 44: 'cot_fmul', 
-    45: 'cot_fdiv', 46: 'cot_fneg', 47: 'cot_neg', 
-    48: 'cot_cast', 49: 'cot_lnot', 50: 'cot_bnot', 
-    51: 'cot_ptr', 52: 'cot_ref', 53: 'cot_postinc', 
-    54: 'cot_postdec', 55: 'cot_preinc', 56: 'cot_predec', 
-    57: 'cot_call', 58: 'cot_idx', 59: 'cot_memref', 
-    60: 'cot_memptr', 61: 'cot_num', 62: 'cot_fnum', 
-    63: 'cot_str', 64: 'cot_obj', 65: 'cot_var', 
-    66: 'cot_insn', 67: 'cot_sizeof', 68: 'cot_helper', 
-    69: 'cot_type', 70: 'cit_empty', 71: 'cit_block', 
-    72: 'cit_expr', 73: 'cit_if', 74: 'cit_for', 
-    75: 'cit_while', 76: 'cit_do', 77: 'cit_switch', 
-    78: 'cit_break', 80: 'cit_return', 81: 'cit_goto', 82: 'cit_asm'
-}
-
-binary_expressions_ops = [
-    idaapi.cot_comma, idaapi.cot_asg, idaapi.cot_asgbor, idaapi.cot_asgxor, idaapi.cot_asgband,
-    idaapi.cot_asgadd, idaapi.cot_asgsub, idaapi.cot_asgmul, idaapi.cot_asgsshr, idaapi.cot_asgushr,
-    idaapi.cot_asgshl, idaapi.cot_asgsdiv, idaapi.cot_asgudiv, idaapi.cot_asgsmod, idaapi.cot_asgumod,
-    idaapi.cot_lor, idaapi.cot_land, idaapi.cot_bor, idaapi.cot_xor, idaapi.cot_band, idaapi.cot_eq,
-    idaapi.cot_ne, idaapi.cot_sge, idaapi.cot_uge, idaapi.cot_sle, idaapi.cot_ule, idaapi.cot_sgt,
-    idaapi.cot_ugt, idaapi.cot_slt, idaapi.cot_ult, idaapi.cot_sshr, idaapi.cot_ushr, idaapi.cot_shl,
-    idaapi.cot_add, idaapi.cot_sub, idaapi.cot_mul, idaapi.cot_sdiv, idaapi.cot_udiv, idaapi.cot_smod,
-    idaapi.cot_umod, idaapi.cot_fadd, idaapi.cot_fsub, idaapi.cot_fmul, idaapi.cot_fdiv
-]
-
-unary_expressions_ops = [
-    idaapi.cot_fneg, idaapi.cot_neg, idaapi.cot_cast, idaapi.cot_lnot, idaapi.cot_bnot, idaapi.cot_ptr,
-     idaapi.cot_ref, idaapi.cot_postinc, idaapi.cot_postdec, idaapi.cot_preinc, idaapi.cot_predec
-]
-
-# [NOTE]: Actual for 7.6
-str2op = {
-	'cot_empty': 0, 'cot_comma': 1, 'cot_asg': 2, 'cot_asgbor': 3, 'cot_asgxor': 4, 'cot_asgband': 5,
-	'cot_asgadd': 6, 'cot_asgsub': 7, 'cot_asgmul': 8, 'cot_asgsshr': 9, 'cot_asgushr': 10,
-	'cot_asgshl': 11, 'cot_asgsdiv': 12, 'cot_asgudiv': 13, 'cot_asgsmod': 14, 'cot_asgumod': 15,
-	'cot_tern': 16, 'cot_lor': 17, 'cot_land': 18, 'cot_bor': 19, 'cot_xor': 20, 'cot_band': 21,
-	'cot_eq': 22, 'cot_ne': 23, 'cot_sge': 24, 'cot_uge': 25, 'cot_sle': 26, 'cot_ule': 27,
-	'cot_sgt': 28, 'cot_ugt': 29, 'cot_slt': 30, 'cot_ult': 31, 'cot_sshr': 32, 'cot_ushr': 33,
-	'cot_shl': 34, 'cot_add': 35, 'cot_sub': 36, 'cot_mul': 37, 'cot_sdiv': 38, 'cot_udiv': 39,
-	'cot_smod': 40, 'cot_umod': 41, 'cot_fadd': 42, 'cot_fsub': 43, 'cot_fmul': 44, 'cot_fdiv': 45,
-	'cot_fneg': 46, 'cot_neg': 47, 'cot_cast': 48, 'cot_lnot': 49, 'cot_bnot': 50, 'cot_ptr': 51,
-	'cot_ref': 52, 'cot_postinc': 53, 'cot_postdec': 54, 'cot_preinc': 55, 'cot_predec': 56,
-	'cot_call': 57, 'cot_idx': 58, 'cot_memref': 59, 'cot_memptr': 60, 'cot_num': 61, 'cot_fnum': 62,
-	'cot_str': 63, 'cot_obj': 64, 'cot_var': 65, 'cot_insn': 66, 'cot_sizeof': 67, 'cot_helper': 68,
-	'cot_type': 69, 'cit_empty': 70, 'cit_block': 71, 'cit_expr': 72, 'cit_if': 73, 'cit_for': 74,
-	'cit_while': 75, 'cit_do': 76, 'cit_switch': 77, 'cit_break': 78, 'cit_return': 80, 'cit_goto': 81, 'cit_asm': 82
-}
 
 def trace_method(method):
     import functools
@@ -99,11 +39,12 @@ class TreeProcessor:
         self.function_tree = cfunc
         self.matcher = matcher
 
-        # Instructions
-        self.op2func = {
+        self.op2func = {i:self.__stub for i in range(100)}
+
+        self.op2func.update({
             idaapi.cit_expr: self._process_cexpr,
-            idaapi.cit_empty: lambda _: None,
-            idaapi.cit_break: lambda _: None,
+            idaapi.cit_empty: self.__stub,
+            idaapi.cit_break: self.__stub,
             idaapi.cit_return: self._process_creturn,
             idaapi.cit_block: self._process_cblock,
             idaapi.cit_if: self._process_cif,
@@ -111,28 +52,19 @@ class TreeProcessor:
             idaapi.cit_while: self._process_cwhile,
             idaapi.cit_do: self._process_cdo,
             idaapi.cit_for: self._process_cfor,
-            idaapi.cit_goto: lambda _: None,
-            idaapi.cit_asm: lambda _: None
-        }
+            idaapi.cit_goto: self.__stub,
+            idaapi.cit_asm: self.__stub
+        })
 
-        # For debugging purposes
-        for i in range(100):
-            if self.op2func.get(i) is None:
-                self.op2func[i] = lambda _: None
-
-        # Basic unary expressions
         for i in unary_expressions_ops:
             self.op2func[i] = self._process_unary_expr
 
-        # Basic binary expressions
         for i in binary_expressions_ops:
             self.op2func[i] = self._process_binary_expr
 
-        # Other expressions
         self.op2func.update({
             idaapi.cot_call: self._process_call_expr
         })
-        
     
     def __assert(self, cond, msg=""):
         assert cond, "%s: %s" % (self.__class__.__name__, msg)
@@ -145,6 +77,8 @@ class TreeProcessor:
 
         self._process_cblock(function_body)
 
+    def __stub(*args, **kwargs) -> None:
+        pass
 
     @trace_method
     def _process_cblock(self, cinsn) -> None:
