0fdedb9fe27cb77ac8a1db7c4bedbc344930c632
mostobriv <mostobriv@gmail.com>
2021-06-24 17:36:39 +0700

    resolve_calling_function_from_node

--- tree/utils.py
+++ tree/utils.py
@@ -3,7 +3,7 @@ import idaapi
 
 # [TODO]: resolving global objects to get their names or at least their addresses
 
-def resolve_obj_address(obj):
+def _resolve_obj_address(obj):
     if obj.op != idaapi.cot_obj:
         return None
     
@@ -13,13 +13,27 @@ def resolve_obj_address(obj):
     raise NotImplementedError("Other methods of resolving address of cot_obj not yet implemented")
 
 
-def resolve_obj_symbol(obj):
-    ea = resolve_obj_address(obj)
+def _resolve_obj_symbol(obj):
+    ea = _resolve_obj_address(obj)
     if ea is None:
         return None
-    
+
     return idaapi.get_name(ea)
 
+
+def resolve_calling_function_from_node(call_node):
+    node = call_node
+    while node.x.op == idaapi.cot_cast:
+        node = node.x
+
+    if node.x.op == idaapi.cot_obj:
+        return _resolve_obj_address(node.x), _resolve_obj_symbol(node.x)
+    
+    elif node.x.op == idaapi.cot_helper:
+        return None, node.x.helper
+
+    else:
+        return None, None
 
 def get_obj_from_call_node(call_node):
     if call_node.x.op == idaapi.cot_cast:
