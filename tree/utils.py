ea609358acc843951573b8f7a35cf9067735e30e
mostobriv <mostobriv@gmail.com>
2021-06-29 21:06:01 +0700

    removed raising of exceptions

--- tree/utils.py
+++ tree/utils.py
@@ -10,8 +10,7 @@ def _resolve_obj_address(obj):
     if obj.obj_ea != idaapi.BADADDR and obj.obj_ea is not None:
         return obj.obj_ea
 
-    raise NotImplementedError("Other methods of resolving address of cot_obj not yet implemented")
-
+    return None
 
 def _resolve_obj_symbol(obj):
     ea = _resolve_obj_address(obj)
@@ -21,7 +20,7 @@ def _resolve_obj_symbol(obj):
     return idaapi.get_name(ea)
 
 
-def resolve_calling_function_from_node(call_node):
+def resolve_calling_function_from_node(call_node) -> tuple(int, str):
     node = call_node
     while node.x.op == idaapi.cot_cast:
         node = node.x
@@ -36,11 +35,11 @@ def resolve_calling_function_from_node(call_node):
         return None, None
 
 def get_obj_from_call_node(call_node):
-    if call_node.x.op == idaapi.cot_cast:
-        if call_node.x.x.op == idaapi.cot_obj:
-            return call_node.x.x
-    elif call_node.x.op == idaapi.cot_obj:
-        return call_node.x
+    node = call_node
+    while node.x.op == idaapi.cot_cast:
+        node = node.x
 
-    raise NotImplementedError("This is not simple (cot_call)->(cot_cast)->(cot_obj) and this case handling not yet implemented")
-    
+    if node.x.op == idaapi.cot_obj:
+        return node.x
+
+    return None
