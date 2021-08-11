2610534457f52251d52d582dc292c24e8cdde256
mostobriv <mostobriv@gmail.com>
2021-08-12 00:18:11 +0700

    simple caching of specific pattern-nodes

--- tree/matcher.py
+++ tree/matcher.py
@@ -1,8 +1,10 @@
 import idaapi
 
 idaapi.require('tree.context')
+idaapi.require('tree.patterns.abstracts')
 
 from tree.context import Context
+from tree.patterns.abstracts import SeqPat
 
 # [TODO]: mb somehow it should have architecture when patterns can provide some more feedback to matcher, not only True/False
 # it can be useful to not traverse every expresion for every pattern-chain, and do it only with a particular-ones
@@ -11,6 +13,7 @@ class Matcher:
     def __init__(self, processed_function):
         self.function = processed_function
         self.patterns = list()
+        self.cache = list()
 
     def check_patterns(self, item):
         for p, h, c in self.patterns:
@@ -27,6 +30,17 @@ class Matcher:
         ctx.update({'current_function': self.function})
         self.patterns.append((pat, handler, ctx))
 
+    def prepare_cache(self):
+        def traverse_pattern(pattern):
+            children = pattern.children
+            for c in children:
+                if isinstance(c, SeqPat):
+                    if c not in self.cache:
+                        self.cache.append(c)
+                traverse_pattern(c)
+        
+        for pattern in self.patterns:
+            traverse_pattern(pattern)
 
     def has_deep_expressions(self):
         for p, _, _ in self.patterns:
