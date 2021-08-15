e5eb6987ce86a6baf806ca001c20880402368358
mostobriv <mostobriv@gmail.com>
2021-08-15 19:47:38 +0700

    no need of such methods atm

--- tree/matcher.py
+++ tree/matcher.py
@@ -4,7 +4,7 @@ idaapi.require('tree.context')
 idaapi.require('tree.patterns.abstracts')
 
 from tree.context import Context
-from tree.patterns.abstracts import SeqPat
+from tree.patterns.abstracts import SeqPat, BindExpr
 
 # [TODO]: mb somehow it should have architecture when patterns can provide some more feedback to matcher, not only True/False
 # it can be useful to not traverse every expresion for every pattern-chain, and do it only with a particular-ones
@@ -17,34 +17,35 @@ class Matcher:
 
     def check_patterns(self, item):
         for p, h, c in self.patterns:
-            if p.check(item) is True:
-                if h is not None:
-                    try:
+            try:
+                if p.check(item, c) is True:
+                    if h is not None:
                         h(item, c)
-                    except Exception as e:
-                        print('[!] Got an exception: %s' % e)
-                # print("[FOUND]: %#x %d" % (item.ea, item.op))
+            except Exception as e:
+                print('[!] Got an exception: %s' % e)
+                raise e
+
 
     def insert_pattern(self, pat, handler=None):
-        ctx = Context()
-        ctx.update({'current_function': self.function})
+        ctx = dict()
+        ctx.update({"current_function_tree": self.function})
         self.patterns.append((pat, handler, ctx))
 
-    def prepare_cache(self):
-        def traverse_pattern(pattern):
-            children = pattern.children
-            for c in children:
-                if isinstance(c, SeqPat):
-                    if c not in self.cache:
-                        self.cache.append(c)
-                traverse_pattern(c)
+    # def prepare_cache(self):
+    #     def traverse_pattern(pattern):
+    #         children = pattern.children
+    #         for c in children:
+    #             if isinstance(c, SeqPat):
+    #                 if c not in self.cache:
+    #                     self.cache.append(c)
+    #             traverse_pattern(c)
         
-        for pattern in self.patterns:
-            traverse_pattern(pattern)
+    #     for pattern in self.patterns:
+    #         traverse_pattern(pattern)
 
     def has_deep_expressions(self):
         for p, _, _ in self.patterns:
-            if p.op >= 0 and p.op < idaapi.cit_empty:
+            if p.op >= 0 and p.op < idaapi.cit_empty or isinstance(p, BindExpr):
                 return True
 
         return False
