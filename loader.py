7de354049b20dae75f1d436d3d17d88046e6a6ce
mostobriv <mostobriv@gmail.com>
2021-09-03 18:18:58 +0700

    initial prototype of patterns management

--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 import idaapi
+import idc
 
 import importlib
 import importlib.util
@@ -7,6 +8,11 @@ import os
 import glob
 import traceback
 import json
+
+idaapi.require('tree.consts')
+from tree.consts import ReadyPatternState
+
+from PyQt5 import QtCore, QtGui
 
 
 def load_module_from_file(path):
@@ -35,54 +41,115 @@ def load_long_str_from_idb(array_name):
     result = [idc.get_array_element(idc.AR_STR, id, idx) for idx in range(max_idx + 1)]
     return b"".join(result).decode("utf-8")
 
-
-class Loader(object):
-    def __init__(self, directory_path):
-        self.directory_path = directory_path
-        self.enabled_patterns = PatternStorage()
-
-        self.load_scripts()
-        # self.apply_db_rules()
-
-    # TODO: isn't it better to reuse spec inside module instead of creating a new one?
-    def reload_enabled(self):
-        for pat in self.enabled_patterns:
-            if not pat.reload():
-                # pat.enabled = False
-
-    def enable_pattern(self, pattern_name):
-        pass
-
-    def disable_pattern(self, pattern_name):
-        pass
-
-    def load_scripts(self):
-        dirpath = os.path.join(os.path.dirname(__file__), self.directory_path)
-        print("Loading files from \"%s\"." % self.directory_path)
-
-        for file_path in glob.glob(dirpath + '/*.py'):
-            try:
-                m = load_module_from_file(file_path)                
-                print("\"%s\" successfully loaded!" % os.path.basename(file_path))
-                self.loaded_modules.append(m)
-            except Exception as e:
-                print("Got an exception due loading of \"%s\" file: %s" % (file_path, e))
-                # traceback.print_exc()
-
-
+def _color_with_opacity(tone, opacity=160):
+    color = QtGui.QColor(tone)
+    color.setAlpha(opacity)
+    return color
 
 def singleton(cls):
     instances = {}
-
-    def get_instance():
+    def getinstance(*args, **kwargs):
         if cls not in instances:
-            instances[cls] = cls()
+            instances[cls] = cls(*args, **kwargs)
         return instances[cls]
-    return get_instance
-
+    return getinstance
 
 @singleton
-class PatternStorage:
+class PatternStorageModel(QtCore.QAbstractListModel):
+    ARRAY_NAME = "$herast:PatternStorage"
+    DEFAULT_DIRECTORY = "ready_patterns"
+    
+    def __init__(self, directory_path=DEFAULT_DIRECTORY, *args):
+        super().__init__(*args)
+        self.ready_patterns = list()
+        self.directory = os.path.join(os.path.dirname(__file__), directory_path)
+        print("[*] Patterns directory: '%s'" % self.directory)
+
+        self._load_patterns()
+    
+    # Qt overload
+    def rowCount(self, parent):
+        return len(self.ready_patterns)
+
+    def data(self, index, role):
+        if not index.isValid():
+            return QtCore.QVariant()
+
+        if index.row() >= len(self.ready_patterns):
+            return QtCore.QVariant()
+
+        pat = self.ready_patterns[index.row()]
+
+        if role == QtCore.Qt.DisplayRole:
+            return QtCore.QVariant(pat.filename)
+
+        elif role == QtCore.Qt.BackgroundRole:
+            if pat.state == ReadyPatternState.ENABLED:
+                return _color_with_opacity(QtCore.Qt.green)
+            elif pat.state == ReadyPatternState.ERROR:
+                return _color_with_opacity(QtCore.Qt.red)
+            elif pat.state == ReadyPatternState.DISABLED:
+                return _color_with_opacity(QtCore.Qt.gray)
+        else:
+            return QtCore.QVariant()
+
+    def dataChanged(self):
+        pass
+
+    # Helper functions
+    def _load_patterns(self):
+        stored_string = load_long_str_from_idb(self.ARRAY_NAME) or b'[]'
+        stored_enabled_array = json.loads(stored_string)
+        if len(stored_enabled_array) == 0:
+            self._cold_load_patterns()
+
+        else:
+            available_files = list(glob.glob(self.directory + '/*.py'))
+            available_basenames = [os.path.basename(full_path) for full_path in available_files]
+            tmp = list(filter(lambda x: x in available_basenames, stored_enabled_array))
+            if len(tmp) != stored_enabled_array:
+                print("[!] Missing some of patterns stored inside IDB, they was excluded.")
+        
+    def _cold_load_patterns(self):
+        print("[*] No patterns were stored inside IDB, performing cold init.")
+
+        for file_path in glob.glob(self.directory + '/*.py'):
+            try:
+                m = load_module_from_file(file_path)
+                state = ReadyPatternState.DISABLED
+                log = "Success!"
+            except Exception as e:
+                m = None
+                state = ReadyPatternState.ERROR
+                log = traceback.format_exc()
+            finally:
+                self.ready_patterns.append(ReadyPattern(file_path, m, state, log))
+        
+    def disable_pattern(self, indices):
+        for qindex in indices:
+            if self.ready_patterns[qindex.row()].state != ReadyPatternState.ERROR:
+                self.ready_patterns[qindex.row()].state = ReadyPatternState.DISABLED
+
+    def enable_pattern(self, indices):
+        for qindex in indices:
+            if self.ready_patterns[qindex.row()].state != ReadyPatternState.ERROR:
+                self.ready_patterns[qindex.row()].state = ReadyPatternState.ENABLED
+
+    def reload_pattern(self, indices):
+        for qindex in indices:
+            if not self.ready_patterns[qindex.row()].reload():
+                del self.ready_patterns[qindex.row()]
+                # emit here that data was changed
+
+
+    def reload_all_patterns(self):
+        pass
+
+    def disable_all_patterns(self):
+        pass
+
+
+class IDBStoring:
     ARRAY_NAME = "$herast:PatternStorage"
 
     def __init__(self):
@@ -112,17 +179,29 @@ class PatternStorage:
             self.__patterns = json.loads(stored_string)
 
 
-
 class ReadyPattern:
-    def __init__(self, path, module, enabled=False):
+    def __init__(self, path, module, state, log):
         self.path = path
         self.filename = os.path.basename(path)
         self.module = module
-        self.enabled = enabled
+        self.state = state
+        self.log = log
+        self.source = str()
+
+        if os.path.isfile(self.path) and os.access(self.path, os.R_OK):
+            with open(self.path, 'r') as f:
+                self.source = f.read()
+        
 
     def reload(self):
         if os.path.isfile(self.path):
-            self.module = load_module_from_file(self.path)
+            try:
+                self.module = load_module_from_file(self.path)
+            except Exception as e:
+                self.module = None
+                self.state = ReadyPatternState.ERROR
+                self.log = traceback.format_exc()
+
             return True
         else:
             return False
---
+++ views/patterns_manager_view.py
@@ -0,0 +1,144 @@
+from PyQt5 import QtCore, QtWidgets, QtGui
+
+import idaapi
+
+
+class ScriptManager(idaapi.PluginForm):
+    def __init__(self, patterns_storage_model):
+        super(ScriptManager, self).__init__()
+        self.patterns_storage_model = patterns_storage_model
+        self.parent = None
+    
+    def OnCreate(self, form):
+        self.parent = idaapi.PluginForm.FormToPyQtWidget(form)
+        self.init_ui()
+
+    def init_ui(self):
+        self.parent.setStyleSheet(
+            "QTableView {background-color: transparent; selection-background-color: #87bdd8;}"
+            "QHeaderView::section {background-color: transparent; border: 0.5px solid;}"
+            "QPushButton {width: 50px; height: 20px;}"
+        )
+        self.parent.resize(400, 600)
+        self.parent.setWindowTitle('HeRAST Patterns View')
+
+        btn_reload = QtWidgets.QPushButton("&Reload")
+        btn_enable = QtWidgets.QPushButton("&Enable")
+        btn_disable = QtWidgets.QPushButton("&Disable")
+        btn_reload_all = QtWidgets.QPushButton("Reload All")
+        btn_disable_all = QtWidgets.QPushButton("Disable All")
+
+        btn_disable.setShortcut('d')
+        btn_enable.setShortcut('e')
+        btn_reload.setShortcut('r')
+        # btn_reload_all.setShortcut('???')
+        # btn_disable_all.setShortcut('???')
+
+
+        patterns_list = QtWidgets.QListView()
+        patterns_list.setModel(self.patterns_storage_model)
+        patterns_list.setMaximumWidth(patterns_list.size().width() // 3)
+
+
+        bottom_btns_grid_box = QtWidgets.QGridLayout()
+        bottom_btns_grid_box.addWidget(btn_reload_all, 0, 0)
+        bottom_btns_grid_box.addWidget(btn_disable_all, 0, 1)
+
+        top_btns_grid_box = QtWidgets.QGridLayout()
+        top_btns_grid_box.addWidget(btn_disable, 0, 0)
+        top_btns_grid_box.addWidget(btn_enable, 0, 1)
+        top_btns_grid_box.addWidget(btn_reload, 0, 2)
+
+        pattern_text_area = PatternSourceView(self.patterns_storage_model)
+        pattern_text_area.setReadOnly(True)
+        # pattern_text_area.setPlainText()
+        
+        loading_log_area = PatternLogView(self.patterns_storage_model)
+        loading_log_area.setReadOnly(True)
+        loading_log_area.setMaximumHeight(100)
+        # loading_log_area.setPlainText()
+
+        splitter = QtWidgets.QSplitter(QtCore.Qt.Vertical)
+        splitter.addWidget(pattern_text_area)
+        splitter.addWidget(loading_log_area)
+
+
+        patterns_list.selectionModel().currentChanged.connect(lambda cur, prev: pattern_text_area.change_source_data(cur, prev))
+        patterns_list.selectionModel().currentChanged.connect(lambda cur, prev: loading_log_area.change_log_data(cur, prev))
+        patterns_list.setCurrentIndex(self.patterns_storage_model.index(0))
+
+        vertical_box = QtWidgets.QVBoxLayout()
+        vertical_box.setSpacing(0)
+        vertical_box.addWidget(splitter)
+        vertical_box.addLayout(top_btns_grid_box)
+        vertical_box.addLayout(bottom_btns_grid_box)
+
+        horizontal_box = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.LeftToRight)
+        horizontal_box.addWidget(patterns_list)
+        horizontal_box.addLayout(vertical_box)
+
+        
+        # [TODO]: after compliting part in module, don't forget to uncomment this lines
+        btn_disable.clicked.connect(lambda: self.patterns_storage_model.disable_pattern(patterns_list.selectedIndexes()))
+        btn_enable.clicked.connect(lambda: self.patterns_storage_model.enable_pattern(patterns_list.selectedIndexes()))
+        # btn_reload.clicked.connect(lambda: self.patterns_storage_model.reload_pattern(patterns_list.selectedIndexes()))
+        # btn_disable_all.clicked.connect(lambda: self.patterns_storage_model.disable_all_patterns())
+        # btn_reload_all.clicked.connect(lambda: self.patterns_storage_model.reload_all_patterns())
+
+        self.parent.setLayout(horizontal_box)
+
+    def OnClose(self, form):
+        pass
+
+    def Show(self, caption=None, options=0):
+        return idaapi.PluginForm.Show(self, caption, options=options)
+
+class PatternSourceView(QtWidgets.QTextEdit):
+    def __init__(self, storage, *args, **kwargs):
+        self.storage = storage
+        super(PatternSourceView, self).__init__(*args, **kwargs)
+
+    def change_source_data(self, current, previous):
+        if current.row() < len(self.storage.ready_patterns):
+            self.setPlainText(self.storage.ready_patterns[current.row()].source)
+        else:
+            self.setPlaintText('')
+
+class PatternLogView(QtWidgets.QTextEdit):
+    def __init__(self, storage, *args, **kwargs):
+        self.storage = storage
+        super(PatternLogView, self).__init__(*args, **kwargs)
+
+    def change_log_data(self, current, previous):
+        if current.row() < len(self.storage.ready_patterns):
+            self.setPlainText(self.storage.ready_patterns[current.row()].log)
+        else:
+            self.setPlaintText('')
+
+
+class ShowScriptManager(idaapi.action_handler_t):
+    description = "Show manager of herast's script's"
+    hotkey = 'Shift+M'
+
+    def __init__(self, model):
+        super(ShowScriptManager, self).__init__()
+        self.model = model
+
+    def update(self, ctx):
+        return True
+
+    def activate(self, ctx):
+        tform = idaapi.find_widget("Script Manager")
+        if tform:
+            tform.activate_widget(tform, True)
+        else:
+            ScriptManager(self.model).Show()
+
+    @property
+    def name(self):
+        return 'herast:' + type(self).__name__    
+
+
+# m = PatternStorageModel()
+# action = ShowScriptManager(m)
+# idaapi.register_action(idaapi.action_desc_t(action.name, action.description, action, action.hotkey))    
