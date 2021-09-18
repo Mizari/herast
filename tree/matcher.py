f2ea03a5765f0eb1dfa6cf609fb9641b813556fe
mostobriv <mostobriv@gmail.com>
2021-09-19 02:06:21 +0700

    microfix

--- tree/matcher.py
+++ tree/matcher.py
@@ -29,8 +29,10 @@ class Matcher:
         self.patterns.append((pat, handler, ctx))
 
     def expressions_traversal_is_needed(self):
+        abstract_expression_patterns = [VarBind, BindExpr]
+        
         for p, _, _ in self.patterns:
-            if p.op >= 0 and p.op < idaapi.cit_empty or isinstance(p, BindExpr, VarBind):
+            if p.op >= 0 and p.op < idaapi.cit_empty or any((isinstance(p, abstract_class) for abstract_class in abstract_expression_patterns)):
                 return True
 
         return False
