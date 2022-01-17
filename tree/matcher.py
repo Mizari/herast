180bd884d8076ccf4d15dd599a1a5f474a917c13
Mizari <gomizari@gmail.com>
2022-01-17 18:57:15 +0700

    added initial api for simpler ctree modifications

--- tree/matcher.py
+++ tree/matcher.py
@@ -1,6 +1,8 @@
 import idaapi
 
-from tree.patterns.abstracts import SeqPat, BindExpr, VarBind
+from tree.patterns.abstracts import BindExpr, VarBind
+import tree.utils as utils
+
 
 class Matcher:
 	def __init__(self, processed_function):
@@ -23,17 +25,61 @@ class Matcher:
 				continue
 
 			try:
-				rv = handler(item, ctx)
-				if not isinstance(rv, bool):
+				is_tree_modified = handler(item, ctx)
+				if not isinstance(is_tree_modified, bool):
 					raise Exception("Handler return invalid return type, should be bool")
 
-				if rv:
+				if is_tree_modified:
 					return True
 			except Exception as e:
 				print('[!] Got an exception during pattern handling: %s' % e)
 				continue
 
+			try:
+				is_tree_modified = self.finalize(ctx)
+			except Exception as e:
+				print('[!] Got an exception during context finalizing: %s' % e)
+				continue
+
+			if is_tree_modified:
+				return True
+
 		return False
+
+	def finalize(self, ctx):
+		tree_changed = False
+		for modified_instr in ctx.modified_instrs():
+			item = modified_instr.item
+			new_item = modified_instr.new_item
+
+			if new_item is None:
+				rv = self.remove_item(ctx, item)
+			else:
+				rv = self.replace_item(ctx, item, new_item)
+			if rv: tree_changed = True
+
+		return tree_changed
+
+	def remove_item(self, ctx, removed_item):
+		parent = ctx.get_parent_block(removed_item)
+		if parent is None:
+			print("[*] Failed to remove item from tree, because no parent is found", removed_item.opname)
+			return False
+
+		rv = utils.remove_instruction_from_ast(removed_item, parent.cinsn)
+		if rv:
+			return True
+		else:
+			print("[*] Failed to remove item from tree")
+			return False
+
+	def replace_item(self, ctx, item, new_item):
+		try:
+			idaapi.qswap(item, new_item)
+			return True
+		except Exception as e:
+			print("[!] Got an exception during ctree instr replacing")
+			return False
 
 	def insert_pattern(self, pat, handler):
 		ctx = SavedContext(self.function)
@@ -47,11 +93,17 @@ class Matcher:
 
 		return False
 
+class InstrModification:
+	def __init__(self, item, new_item):
+		self.item = item
+		self.new_item = new_item
+
 class SavedContext:
 	def __init__(self, current_function):
 		self.current_function = current_function
 		self.expressions = dict()
 		self.variables = dict()
+		self.instrs_to_modify = []
 
 	def get_var(self, name):
 		return self.variables.get(name, None)
@@ -74,6 +126,20 @@ class SavedContext:
 	def cleanup(self):
 		self.variables.clear()
 		self.expressions.clear()
+		self.instrs_to_modify.clear()
+
+	def modify_instr(self, item, new_item):
+		self.instrs_to_modify.append(InstrModification(item, new_item))
+
+	def modified_instrs(self):
+		for itm in self.instrs_to_modify:
+			yield itm
+
+	def get_parent_block(self, item):
+		parent = self.current_function.body.find_parent_of(item)
+		if parent is None or parent.op != idaapi.cit_block:
+			return None
+		return parent
 
 class SavedVariable:
 	def __init__(self, idx):
