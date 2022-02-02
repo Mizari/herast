a4a80fa585f0f5eaec302870e0aad0392346b0f6
Mizari <gomizari@gmail.com>
2022-02-02 15:43:19 +0700

    unnecessary trycatch here

--- ready_patterns/call_explore.py
+++ ready_patterns/call_explore.py
@@ -10,13 +10,8 @@ from tree.utils import *
 test_pattern = ExInsPat(CallExprPat('_objc_release', ignore_arguments=True))
 
 def test_handler(item, ctx):
-	try:
-		print("[FOUND]: %#x" % item.ea)
-		ctx.modify_instr(item, None)
-		return False
-	except Exception as e:
-		print('Got an exception due handling: %s' % e)
-
+	print("[FOUND]: %#x" % item.ea)
+	ctx.modify_instr(item, None)
 	return False
 
 
