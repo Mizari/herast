56acf4189ece7c83a47cdd41878400a9fe086ba5
Mizari <gomizari@gmail.com>
2022-02-02 15:53:55 +0700

    refactoring

--- ready_patterns/wasm_str_lit_cref_definer.py
+++ ready_patterns/wasm_str_lit_cref_definer.py
@@ -1,13 +1,8 @@
 import idaapi
 
-idaapi.require('tree.patterns.abstracts')
-idaapi.require('tree.patterns.expressions')
-idaapi.require('tree.patterns.instructions')
-idaapi.require('tree.utils')
-
 from tree.patterns.abstracts import *
-from tree.patterns.expressions import CallExprPat, AsgExprPat, ObjPat, AsgExprPat, AsgxorExprPat
-from tree.patterns.instructions import ExInsPat, IfInsPat, BlockPat, ForInsPat
+from tree.patterns.expressions import ObjPat
+from tree.patterns.instructions import ExInsPat
 
 from tree.utils import *
 
@@ -18,7 +13,7 @@ def handler(item, ctx):
 	obj = ctx.get_expr('xref_me')
 	if obj.obj_ea >= 0 and obj.obj_ea <= 0x100000:
 		idaapi.add_dref(obj.ea, obj.obj_ea, idaapi.dr_O)
-	
+
 	return False
 
 
