f24db3d99b21143a61a3f578de0eab0cbc9c0312
Max Stanchin <15954313+mostobriv@users.noreply.github.com>
2022-05-23 15:27:15 +0700

    Update storage_manager_view.py

--- herast/views/storage_manager_view.py
+++ herast/views/storage_manager_view.py
@@ -10,97 +10,6 @@ import herast.storage_manager as storage_manager
 from typing import Optional
 
 
-"""
-class StorageManagerForm(idaapi.PluginForm):
-	def __init__(self, storage_manager_model: StorageManager):
-		super(StorageManagerForm, self).__init__()
-		self.storage_manager_model = storage_manager_model
-		self.parent = None
-	
-	def OnCreate(self, form):
-		self.parent = idaapi.PluginForm.FormToPyQtWidget(form)
-		self.init_ui()
-
-	def init_ui(self):
-		self.parent.setStyleSheet(
-			"QTableView {background-color: transparent; selection-background-color: #87bdd8;}"
-			"QHeaderView::section {background-color: transparent; border: 0.5px solid;}"
-			"QPushButton {width: 50px; height: 20px;}"
-		)
-		self.parent.resize(400, 600)
-		self.parent.setWindowTitle('HeRAST Scheme Storages View')
-
-		btn_reload = QtWidgets.QPushButton("&Reload")
-		btn_enable = QtWidgets.QPushButton("&Enable")
-		btn_disable = QtWidgets.QPushButton("&Disable")
-		btn_refresh_all = QtWidgets.QPushButton("Refresh all")
-		btn_disable_all = QtWidgets.QPushButton("Disable All")
-
-		btn_disable.setShortcut('d')
-		btn_enable.setShortcut('e')
-		btn_reload.setShortcut('r')
-		# btn_refresh.setShortcut('???')
-		# btn_disable_all.setShortcut('???')
-
-		storages_list = QtWidgets.QListView()
-		storages_list.setModel(self.storage_manager_model)
-		storages_list.setMaximumWidth(storages_list.size().width() // 3)
-		storages_list.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
-
-		bottom_btns_grid_box = QtWidgets.QGridLayout()
-		bottom_btns_grid_box.addWidget(btn_refresh_all, 0, 0)
-		bottom_btns_grid_box.addWidget(btn_disable_all, 0, 1)
-
-		top_btns_grid_box = QtWidgets.QGridLayout()
-		top_btns_grid_box.addWidget(btn_disable, 0, 0)
-		top_btns_grid_box.addWidget(btn_enable, 0, 1)
-		top_btns_grid_box.addWidget(btn_reload, 0, 2)
-
-		storage_text_area = StorageSourceView(storages_list.model())
-		storage_text_area.setReadOnly(True)
-
-		loading_log_area = StorageLogView(storages_list.model())
-		loading_log_area.setReadOnly(True)
-		loading_log_area.setMaximumHeight(100)
-
-		splitter = QtWidgets.QSplitter(QtCore.Qt.Vertical)
-		splitter.addWidget(storage_text_area)
-		splitter.addWidget(loading_log_area)
-
-		storages_list.selectionModel().currentChanged.connect(lambda cur, prev: storage_text_area.switch_source_data(cur, prev))
-		storages_list.selectionModel().currentChanged.connect(lambda cur, prev: loading_log_area.switch_log_data(cur, prev))
-		storages_list.setCurrentIndex(storages_list.model().index(0))
-
-		storages_list.model().dataChanged.connect(lambda start, end: storage_text_area.reload_source_data(start, end, storages_list.selectedIndexes()))
-		storages_list.model().dataChanged.connect(lambda start, end: loading_log_area.reload_log_data(start, end, storages_list.selectedIndexes()))
-
-		vertical_box = QtWidgets.QVBoxLayout()
-		vertical_box.setSpacing(0)
-		vertical_box.addWidget(splitter)
-		vertical_box.addLayout(top_btns_grid_box)
-		vertical_box.addLayout(bottom_btns_grid_box)
-
-		horizontal_box = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.LeftToRight)
-		horizontal_box.addWidget(storages_list)
-		horizontal_box.addLayout(vertical_box)
-
-		btn_disable.clicked.connect(lambda: storages_list.model().disable_storage(storages_list.selectedIndexes()))
-		btn_enable.clicked.connect(lambda: storages_list.model().enable_storage(storages_list.selectedIndexes()))
-		btn_reload.clicked.connect(lambda: storages_list.model().reload_storage(storages_list.selectedIndexes()))
-		btn_disable_all.clicked.connect(lambda: storages_list.model().disable_all_storages())
-
-		btn_refresh_all.clicked.connect(lambda: storages_list.model().refresh_storages())
-
-		self.parent.setLayout(horizontal_box)
-
-	def OnClose(self, form):
-		pass
-
-	def Show(self, caption=None, options=0):
-		return idaapi.PluginForm.Show(self, caption, options=options)
-"""
-
-
 def _color_with_opacity(tone, opacity=160):
 	color = QtGui.QColor(tone)
 	color.setAlpha(opacity)
@@ -120,6 +29,7 @@ class SchemeStorageTreeItem:
 		self.type = type
 		self.parent = parent 
 		self.fullpath = None
+		self.enabled = False
 
 	def parentItem(self):
 		return self.parent
@@ -156,6 +66,17 @@ class SchemeStorageTreeItem:
 	def is_file(self):
 		return not self.is_directory()
 
+	def enable(self):
+		self.enabled = True
+
+	def disable(self):
+		self.enabled = False
+
+	def is_enabled(self):
+		if self.is_file() and self.enabled:
+			return True
+		else:
+			return False
 
 @singleton
 class StorageManagerModel(QtCore.QAbstractItemModel):
@@ -211,16 +132,20 @@ class StorageManagerModel(QtCore.QAbstractItemModel):
 		if not index.isValid():
 			return QtCore.QVariant()
 
+		if role == QtCore.Qt.BackgroundRole:
+			item = self.get_item(index)
+			if item is None or item.is_directory():
+				return QtCore.QVariant()
+		
+			if item.is_file() and item.is_enabled():
+				return _color_with_opacity(QtCore.Qt.green)
+			else:
+				return _color_with_opacity(QtCore.Qt.gray, opacity=80)
+
 		if role != QtCore.Qt.DisplayRole:
 			return QtCore.QVariant()
 
 		item: SchemeStorageTreeItem = self.get_item(index)
-
-		if role == QtCore.Qt.BackgroundRole:
-			if item.is_file():
-				return _color_with_opacity(QtCore.Qt.Green)
-			else:
-				return _color_with_opacity(QtCore.Qt.gray)
 
 		return item.data(index.column())
 
@@ -265,17 +190,39 @@ class StorageManagerModel(QtCore.QAbstractItemModel):
 			return None
 		return storage_manager.get_storage(item.fullpath)
 
+	def _get_item_by_index(self, idx):
+		item = self.get_item(idx)
+		if item.fullpath is None:
+			return None
+
+		return item
+
+	def _get_file_item_by_index(self, idx):
+		item = self._get_item_by_index(idx)
+		if item is None or not item.is_file():
+			return None
+		
+		return item
+
 	def disable_storage(self, indices):
 		for qindex in indices:
 			storage = self.get_storage_by_index(qindex)
 			if storage is not None:
 				storage.disable()
+				item = self._get_file_item_by_index(qindex)
+				if item is not None:
+					item.disable()
+					self.dataChanged.emit(qindex, qindex)
 
 	def enable_storage(self, indices):
 		for qindex in indices:
 			storage = self.get_storage_by_index(qindex)
 			if storage is not None:
 				storage.enable()
+				item = self._get_file_item_by_index(qindex)
+				if item is not None:
+					item.enable()
+					self.dataChanged.emit(qindex, qindex)
 
 	def reload_storage(self, indices):
 		for qindex in indices:
