100b67cc750f911acb490d180ba35f7653cc5877
Mizari <gomizari@gmail.com>
2022-06-16 21:29:14 +0700

    refactoring

--- herast/ready_patterns/virtual_collector.py
+++ herast/ready_patterns/virtual_collector.py
@@ -1,50 +1,26 @@
-import idaapi
-from herast.tree.patterns.abstracts import OrPat, PatternContext, SkipCasts, AnyPat, StructMemptr
-from herast.tree.patterns.expressions import AsgExprPat, CallExprPat, ObjPat
-from herast.tree.patterns.instructions import ExInsPat
-from herast.tree.matcher import Matcher
-
-from herast.tree.utils import *
-
-from herast.schemes.single_pattern_schemes import SPScheme
+import herapi
+import idautils
 
 
-def get_func_start(addr):
-	func = idaapi.get_func(addr)
-	if func is None:
-		return idaapi.BADADDR
-	return func.start_ea
-
-class VirtualCollector(SPScheme):
+class VirtualCollector(herapi.SPScheme):
 	def __init__(self, struct_type=None, offset=None):
-		pattern = AsgExprPat(StructMemptr(struct_type, offset), AnyPat())
+		pattern = herapi.AsgExprPat(herapi.StructMemptr(struct_type, offset), herapi.AnyPat())
 		super().__init__("virtual_collector", pattern)
 		self.collection = []
 
-	def on_matched_item(self, item, ctx: PatternContext):
-		func_ea = get_func_start(item.ea)
+	def on_matched_item(self, item, ctx: herapi.PatternContext):
+		func_ea = herapi.get_func_start(item.ea)
 		struct_type = item.x.x.type.get_pointed_object()
 		offset = item.x.m
 		value = item.y
 		self.collection.append((func_ea, struct_type, offset, value))
 		return False
 
-def get_cfunc(func_ea):
-	try:
-		cfunc = idaapi.decompile(func_ea)
-	except:
-		print("Error: failed to decompile function {x}".format(hex(func_ea)))
-		return None
-
-	if cfunc is None:
-		print("Error: failed to decompile function {x}".format(hex(func_ea)))
-	return cfunc
-
 def collect_virtual_properties(functions=idautils.Functions(), struct_type=None, offset=None):
 	scheme = VirtualCollector(struct_type, offset)
-	matcher = Matcher(scheme)
+	matcher = herapi.Matcher(scheme)
 
-	cfuncs = {ea: get_cfunc(ea) for ea in functions}
+	cfuncs = {ea: herapi.get_cfunc(ea) for ea in functions}
 	for ea, cfunc in cfuncs.items():
 		matcher.match_cfunc(cfunc)
 
