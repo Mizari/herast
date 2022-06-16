932f770f683b6d4d5c39a4d04e749e679bbfa1cb
Mizari <gomizari@gmail.com>
2022-06-16 21:25:17 +0700

    added useful functions

--- herapi.py
+++ herapi.py
@@ -1,3 +1,4 @@
+import idaapi
 import herast.storage_manager as storage_manager
 
 def get_storages():
@@ -5,3 +6,20 @@ def get_storages():
 
 def get_storage(storage_path):
 	return storage_manager.get_storage(storage_path)
+
+def get_func_start(addr):
+	func = idaapi.get_func(addr)
+	if func is None:
+		return idaapi.BADADDR
+	return func.start_ea
+
+def get_cfunc(func_ea):
+	try:
+		cfunc = idaapi.decompile(func_ea)
+	except:
+		print("Error: failed to decompile function {x}".format(hex(func_ea)))
+		return None
+
+	if cfunc is None:
+		print("Error: failed to decompile function {x}".format(hex(func_ea)))
+	return cfunc
