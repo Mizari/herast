5cb1352a4d0838644c88495e386d65bff5706dd0
Mizari <gomizari@gmail.com>
2022-06-16 21:29:08 +0700

    added forward api

--- herapi.py
+++ herapi.py
@@ -1,5 +1,13 @@
 import idaapi
+
 import herast.storage_manager as storage_manager
+
+# forward imports for herast usage
+from herast.tree.patterns.abstracts import *
+from herast.tree.patterns.expressions import *
+from herast.tree.patterns.instructions import *
+from herast.tree.matcher import Matcher
+from herast.schemes.single_pattern_schemes import SPScheme
 
 def get_storages():
 	return [s for s in storage_manager.schemes_storages.values()]
