0095dfa5965b8e373d007ab23bf7bf80d6b63b15
Mizari <gomizari@gmail.com>
2022-06-27 19:55:28 +0700

    expanded herapi

--- herapi.py
+++ herapi.py
@@ -5,7 +5,7 @@ from herast.tree.patterns.abstracts import *
 from herast.tree.patterns.expressions import *
 from herast.tree.patterns.instructions import *
 from herast.tree.matcher import Matcher
-from herast.schemes.single_pattern_schemes import SPScheme
+from herast.schemes.single_pattern_schemes import SPScheme, ItemRemovalScheme
 from herast.herast_settings import get_herast_enabled, get_herast_files, get_herast_folders, add_herast_enabled, add_herast_file, add_herast_folder
 from herast.idb_settings import get_enabled_idb, get_idb_files, get_idb_folders, add_idb_enabled, add_idb_file, add_idb_folder
 
