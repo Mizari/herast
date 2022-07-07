f0a3513686c009bea69cc5c32434086c6dc535ae
Mizari <gomizari@gmail.com>
2022-07-07 12:51:55 +0700

    refactoring

--- examples/passives/sharedptr.py
+++ examples/passives/sharedptr.py
@@ -1,6 +1,18 @@
 from herapi import *
 
-SHOULD_REMOVE = True
+class HelperReplacer(SPScheme):
+	def __init__(self, name, pattern, helper_name, should_remove=False):
+		self.helper_name = helper_name
+		self.should_remove = should_remove
+		super().__init__(name, pattern)
+
+	def on_matched_item(self, item, ctx: PatternContext):
+		if self.should_remove:
+			new_item = None
+		else:
+			new_item = make_call_helper_instr(self.helper_name)
+		ctx.modify_instr(item, new_item)
+		return False
 
 
 release_pattern = SeqPat(
@@ -38,39 +50,26 @@ release_pattern = SeqPat(
 	)),
 )
 
-class HelperReplacer(SPScheme):
-	def __init__(self, name, pattern, helper_name):
-		self.helper_name = helper_name
-		super().__init__(name, pattern)
-	
-	def on_matched_item(self, item, ctx: PatternContext):
-		if SHOULD_REMOVE:
-			new_item = None
-		else:
-			new_item = make_call_helper_instr(self.helper_name)
-		ctx.modify_instr(item, new_item)
-		return False
-
 register_storage_scheme(HelperReplacer("shptr_release", release_pattern, "__sharedptr::release"))
 
 
 increment_pattern = IfPat(
 	ObjPat("pthread_cancel"),
-	CallInsnPat(HelperPat(helper_name="_InterlockedAdd"), skip_missing=True),
+	CallInsnPat(HelperPat("_InterlockedAdd"), skip_missing=True),
 	ExprInsPat(),
 )
 
 increment_pattern = OrPat(
-	increment_pattern,
 	IfPat(AnyPat(), increment_pattern),
+	increment_pattern,
 )
 register_storage_scheme(HelperReplacer("shptr_inc", increment_pattern, "__sharedptr::increment"))
 
+
 fname = "std::_Sp_counted_base::_M_release"
 std_release_pattern = OrPat(
-	CallInsnPat(fname, ignore_arguments=True),
 	IfPat(AnyPat(), CallInsnPat(fname, ignore_arguments=True)),
+	CallInsnPat(fname, ignore_arguments=True),
 )
 
-if SHOULD_REMOVE:
-	register_storage_scheme(SPScheme("shptr_release_remover", RemovePattern(std_release_pattern)))
+register_storage_scheme(ItemRemovalScheme("shptr_release_remover", RemovePattern(std_release_pattern)))
