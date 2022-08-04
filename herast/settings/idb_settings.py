63666629d72e355e0dea6a817cf683032c44a45f
Mizari <gomizari@gmail.com>
2022-08-04 12:45:48 +0700

    added warning

--- herast/settings/idb_settings.py
+++ herast/settings/idb_settings.py
@@ -5,10 +5,13 @@ from herast.settings.base_settings import BaseSettings
 def load_long_str_from_idb(array_name):
 	id = idc.get_array_id(array_name)
 	if id == -1:
+		print("Failed to get array id with name", array_name)
 		return None
+
 	max_idx = idc.get_last_index(idc.AR_STR, id)
 	result = [idc.get_array_element(idc.AR_STR, id, idx) for idx in range(max_idx + 1)]
-	return b"".join(result).decode("utf-8")
+	result = b"".join(result)
+	return result.decode("utf-8")
 
 
 def save_long_str_to_idb(array_name, value):
