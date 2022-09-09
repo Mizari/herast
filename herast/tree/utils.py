3d8883d0b991cdc664c2a8f7f9f226b8e531501b
Mizari <gomizari@gmail.com>
2022-09-09 16:28:57 +0700

    more dead code

--- herast/tree/utils.py
+++ herast/tree/utils.py
@@ -3,37 +3,6 @@ import idc
 import idautils
 
 
-def _resolve_obj_address(obj):
-	if obj.op != idaapi.cot_obj:
-		return None
-
-	if obj.obj_ea != idaapi.BADADDR and obj.obj_ea is not None:
-		return obj.obj_ea
-
-	return None
-
-def _resolve_obj_symbol(obj):
-	ea = _resolve_obj_address(obj)
-	if ea is None:
-		return None
-
-	return idaapi.get_name(ea)
-
-
-def resolve_calling_function_from_node(call_node):
-	node = call_node
-	while node.x.op == idaapi.cot_cast:
-		node = node.x
-
-	if node.x.op == idaapi.cot_obj:
-		return _resolve_obj_address(node.x), _resolve_obj_symbol(node.x)
-	
-	elif node.x.op == idaapi.cot_helper:
-		return None, node.x.helper
-
-	else:
-		return None, None
-
 def get_obj_from_call_node(call_node):
 	node = call_node
 	while node.x.op == idaapi.cot_cast:
