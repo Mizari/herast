4095f19cdb8fb33be9ed4952635e3ee51c4983cb
Mizari <gomizari@gmail.com>
2023-11-28 11:34:40 +0700

    bugfix

--- examples/passives/cplusplus/sharedptr.py
+++ examples/passives/cplusplus/sharedptr.py
@@ -13,11 +13,21 @@ class HelperReplacer(Scheme):
 		super().__init__(pattern)
 
 	def on_matched_item(self, item, ctx: MatchContext) -> ASTPatch|None:
-		if self.should_remove:
-			return ASTPatch.remove_instr(item)
+		remove_me = ctx.get_item("remove_me")
+		res = remove_instr(remove_me, ctx)
+		if remove_me is None or res is None:
+			if self.should_remove:
+				return ASTPatch.remove_instr(item)
 
-		new_item = make_call_helper_instr(self.helper_name)
-		return ASTPatch.replace_instr(item, new_item)
+			new_item = make_call_helper_instr(self.helper_name)
+			return ASTPatch.replace_instr(item, new_item)
+
+		if self.should_remove:
+			remove_instr(item, ctx)
+		else:
+			new_item = make_call_helper_instr(self.helper_name)
+			replace_instr(item, new_item, ctx)
+		return ASTPatch.scheme_modified()
 
 """
 	A rather complex pattern representing releasing of C++'s sharedptr
@@ -79,9 +89,10 @@ release_pattern = \
 """
 	this patterns looks for two instructions
 	first one will later be replaced/removed in HelperReplacer
-	second one will be deleted via Remove Pattern
+	second one will be deleted via bind_name search
 """
-release_pattern = SeqPat(AsgInsnPat(VarPat(), AnyPat()), RemovePat(release_pattern))
+release_pattern.bind_name = "remove_me"
+release_pattern = SeqPat(AsgInsnPat(VarPat(), AnyPat()), release_pattern)
 register_storage_scheme("shptr_release", HelperReplacer(release_pattern, "__sharedptr::release"))
 
 def cond_insn(pat):
@@ -129,4 +140,4 @@ std_release_pattern = cond_insn(CallInsnPat(
 									"std::_Sp_counted_base::_M_release",
 									ignore_arguments=True
 								))
-register_storage_scheme("shptr_release_remover", ItemRemovalScheme(RemovePat(std_release_pattern)))
+register_storage_scheme("shptr_release_remover", ItemRemovalScheme(std_release_pattern))
