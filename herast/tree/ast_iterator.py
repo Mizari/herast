f62c6ba4d3a17d8abb2157417f5d7b889d0deb76
Mizari <gomizari@gmail.com>
2023-11-29 14:32:34 +0700

    new data structure for ast iteration

--- herast/tree/ast_iterator.py
+++ herast/tree/ast_iterator.py
@@ -1,29 +1,45 @@
 from __future__ import annotations
-from herast.tree.ast_iteration import iterate_all_subitems, IterationBreak
+from herast.tree.ast_iteration import IterationBreak, get_children
 from herast.tree.ast_patch import ASTPatch
 from herast.tree.ast_context import ASTContext
 
 
+def build_path(ast):
+	path = []
+	while len(children := get_children(ast)) != 0:
+		path.append((ast,0))
+		ast = children[0]
+	path.append((ast, -1))
+	return path
+
+
 class ASTIterator:
-	def __init__(self, ast):
-		self.ast = ast
-		self.cor = iterate_all_subitems(self.ast)
+	def __init__(self, root):
+		self.root = root
+		self.path = build_path(root)
 
 	def get_next(self):
-		try:
-			return self.cor.__next__()
-		except StopIteration:
+		if len(self.path) == 0:
 			return None
+
+		current_item, child_idx = self.path.pop()
+		if child_idx == -1: # -1 means no need to iterate children
+			return current_item
+
+		children = get_children(current_item)
+		if len(children) == child_idx + 1:
+			return current_item
+
+		self.path.append((current_item, child_idx+1))
+		child = children[child_idx+1]
+		self.path += build_path(child)
+		return self.get_next()
 
 	def break_iteration(self, itbreak:IterationBreak):
 		if itbreak is IterationBreak.ROOT:
-			self.cor = iterate_all_subitems(self.ast)
+			self.path = build_path(self.root)
 		else:
 			raise ValueError("Not implemented")
-
-	def iterate_subitems(self):
-		while (sub := self.get_next()) is not None:
-			yield sub
 
 	def apply_patch(self, ast_patch:ASTPatch, ast_ctx:ASTContext) -> IterationBreak:
 		itbreak = ast_patch.do_patch(ast_ctx)
