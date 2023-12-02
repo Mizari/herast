4fc87d1b8ec2da384dbec7492e9412682fe40f02
Mizari <gomizari@gmail.com>
2023-12-02 11:20:04 +0700

    refactoring

--- herast/tree/ast_patch.py
+++ herast/tree/ast_patch.py
@@ -7,8 +7,11 @@ from herast.tree.ast_context import ASTContext
 
 
 def remove_instr(item:idaapi.cinsn_t, ctx:ASTContext) -> bool:
-	gotos = collect_gotos(item)
-	if len(gotos) > 0:
+	removed_gotos = collect_gotos(item)
+	if item.op == idaapi.cit_goto:
+		removed_gotos.append(item)
+
+	if len(removed_gotos) > 0:
 		print("[!] failed removing item with gotos in it")
 		return False
 
@@ -17,29 +20,23 @@ def remove_instr(item:idaapi.cinsn_t, ctx:ASTContext) -> bool:
 		print("[*] Failed to remove item from tree, because no parent is found", item.opname)
 		return False
 
-	labels = collect_labels(item)
-	if len(labels) == 1 and labels[0] == item:
-		next_item = utils.get_following_instr(parent, item)
-		if next_item is None:
-			print("[!] failed2removing item with labels in it", next_item)
-			return False
+	removed_labels = collect_labels(item)
+	if item.label_num != -1:
+		removed_labels.append(item.label_num)
 
-	elif len(labels) > 0:
+	if len(removed_labels) > 0:
 		print("[!] failed removing item with labels in it")
 		return False
 
-	saved_lbl = item.label_num
-	item.label_num = -1
-	rv = utils.remove_instruction_from_ast(item, parent.cinsn)
-	if not rv:
-		item.label_num = saved_lbl
-		print(f"[*] Failed to remove item {item.opname} from tree at {hex(item.ea)}")
+	next_item = utils.get_following_instr(parent, item)
+	if next_item is None:
+		print("[!] failed2removing item with labels in it", next_item)
 		return False
 
-	next_item = utils.get_following_instr(parent, item)
-	if next_item is not None:
-		next_item.label_num = saved_lbl
-	return True
+	rv = utils.remove_instruction_from_ast(item, parent.cinsn)
+	if not rv:
+		print(f"[*] Failed to remove item {item.opname} from tree at {hex(item.ea)}")
+	return rv
 
 def replace_instr(item, new_item:idaapi.cinsn_t, ctx:ASTContext) -> bool:
 	gotos = collect_gotos(item)
